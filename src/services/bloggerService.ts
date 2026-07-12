e3c969f412c0e3996f408cdd7e7bdb130d300623
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-12 10:04:10 +0530

    Update bloggerService.ts

--- src/services/bloggerService.ts
+++ src/services/bloggerService.ts
@@ -333,9 +333,9 @@ if (fetchedArticles.length === 0) {
   
 try {
   const res = await fetch(
-"https://www.flashnews24.site/feeds/posts/default?alt=json&max-results=500",
+"https://www.google.com",
 {
-  cache: "no-store"
+  cache: "no-store",
 }
 );
   alert("STATUS = " + res.status);
