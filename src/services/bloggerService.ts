3ea5a0789bfb2181d58969dc466689780a2bae80
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-13 00:30:59 +0530

    Update bloggerService.ts

--- src/services/bloggerService.ts
+++ src/services/bloggerService.ts
@@ -1,8 +1,7 @@
 import { Article, NewsCategory } from '../types';
 
-export const BLOGGER_JSON_FEED_URL =
-"https://www.flashnews24.site/feeds/posts/default?alt=json&max-results=500";
-alert(BLOGGER_JSON_FEED_URL);
+export const BLOGGER_SITE_URL = 'https://www.flashnews24.site';
+export const BLOGGER_JSON_FEED_URL = `${BLOGGER_SITE_URL}/feeds/posts/default?alt=json&max-results=500`;
 
 /**
  * Decodes standard HTML entities in Blogger text payloads.
