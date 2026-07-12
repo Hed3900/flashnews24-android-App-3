e22518bc5d597c3f781d3e2b681117f0e4c9de89
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-13 03:20:41 +0530

    Update HomeScreen.tsx

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -279,7 +279,6 @@ const regularArticles = articles
   src={art.imageUrl}
   alt={art.title}
   onError={() => alert(art.imageUrl)}
-  decoding="async"
   className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
 />
                         <div className="absolute inset-0 bg-gradient-to-t from-[#0F1115] via-[#0F1115]/40 to-transparent"></div>
