55d336e10d8220c02964833b52010b3526c914f6
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-14 09:39:15 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.flashnews24.app',
-  appName: 'FlashNews24',
+  appId: "com.flashnews24.app",
+  appName: "FlashNews24",
+  webDir: "dist",
   server: {
     androidScheme: "http",
     cleartext: true
-}
+  }
+};
   plugins: {
     PushNotifications: {
       presentationOptions: ["badge", "sound", "alert"],
