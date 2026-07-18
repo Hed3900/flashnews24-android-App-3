91c68be4d85cc1bc6e6a524d6df1654c53138d39
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-18 17:02:23 +0530

    Update MainActivity.java

--- android/app/src/main/java/io/flashnews24/app/MainActivity.java
+++ android/app/src/main/java/io/flashnews24/app/MainActivity.java
@@ -1,11 +1,18 @@
 package com.flashnews24.app;
 
 import android.os.Bundle;
+import android.content.Intent;
 
 import com.getcapacitor.BridgeActivity;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
