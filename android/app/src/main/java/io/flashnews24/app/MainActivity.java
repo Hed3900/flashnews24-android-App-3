d87b20c5a760b74ad40071a6d6ce5e53945bc75b
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-23 18:35:47 +0530

    Update MainActivity.java

--- android/app/src/main/java/io/flashnews24/app/MainActivity.java
+++ android/app/src/main/java/io/flashnews24/app/MainActivity.java
@@ -2,11 +2,12 @@ package com.flashnews24.app;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
 import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.messaging.FirebaseMessaging;
-import android.util.Log;
+
 public class MainActivity extends BridgeActivity {
 
     @Override
@@ -20,22 +21,16 @@ public class MainActivity extends BridgeActivity {
         super.onCreate(savedInstanceState);
 
         MobileAds.initialize(this, initializationStatus -> {});
+
+        FirebaseMessaging.getInstance().getToken()
+            .addOnCompleteListener(task -> {
+                if (!task.isSuccessful()) {
+                    Log.w("FCM", "Fetching FCM registration token failed", task.getException());
+                    return;
+                }
+
+                String token = task.getResult();
+                Log.d("FCM_TOKEN", token);
+            });
     }
-}
-@Override
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    MobileAds.initialize(this, initializationStatus -> {});
-
-    FirebaseMessaging.getInstance().getToken()
-        .addOnCompleteListener(task -> {
-            if (!task.isSuccessful()) {
-                Log.w("FCM", "Fetching FCM registration token failed", task.getException());
-                return;
-            }
-
-            String token = task.getResult();
-            Log.d("FCM_TOKEN", token);
-        });
 }
